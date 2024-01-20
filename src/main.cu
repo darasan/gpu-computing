05fac2a920d0fd66dccac1ae8b668dc2b39486f0
darasan <oneill.daire@gmail.com>
2024-01-20 16:35:17 +0100

    Updated main with basic read/write of image files. Added STB headers and test images

--- src/main.cu
+++ src/main.cu
@@ -10,12 +10,51 @@
 #include <stdlib.h>
 #include "SIFT_CUDA.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h" 
+
+const char* filename = "../img/chartreuse.jpg";
 
 int main(int argc, char **argv) {
 
-    SIFT_CUDA sift;
+    //Load image
+    int inputWidth, inputHeight, inputChannels;
+  
+    unsigned char *inputData = stbi_load(filename, &inputWidth, &inputHeight, &inputChannels, 0);
+    int inputImageSize = (inputWidth * inputHeight * inputChannels);
 
-    sift.display();
+    if(inputData == NULL){
+        std::cout << "Error loading images\n" << std::endl;
+        exit(1);
+    }
 
-    return 0;
+    //Calculate num channels and size for output file
+    int outputChannels = inputChannels = 3;
+    int outputWidth = inputWidth;
+    int outputHeight = inputHeight;
+    int outputImageSize = (outputWidth * outputHeight * outputChannels);
+
+    unsigned char *outputData = new unsigned char[outputImageSize];
+    if(outputData == NULL) {
+        std::cout << "Unable to allocate memory for output image\n" << std::endl;
+        exit(1);
+    }
+
+    //Write result to file
+    std::cout <<"Write to file\n" << std::endl;
+    int success = stbi_write_jpg("output.jpg", outputWidth, outputHeight, outputChannels, inputData, 100);
+    if(success){
+        std::cout <<"Wrote file OK! w:" << outputWidth << " h: " << outputHeight << "chans: " << outputChannels << std::endl;
+    }
+
+    else{
+        std::cout <<"Error writing output file\n" << std::endl;
+    }
+
+    stbi_image_free(inputData);
+    delete(outputData);
+
+    exit(0);
 }
